bc2da5f372c283cdc9edfe19fb3f56c1fb80d571
TheRoktor <rh3430@student.uni-lj.si>
2020-01-08 00:23:06 +0100

    basic site template done

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-REHJTs1r2ErKBuJB0fCK99gCYsVjwxHrSU0N7I1zl9vZbggVJXRMsv/sLlOAGb4M" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('src/app.css') }}" >
+
+    @yield('styles')
+</head>
+<body>
+@include('partials.header')
+
+<div class="container-fluid" >
+    @yield('content')
+</div>
+
+<script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+@yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.master')
+
+@section('title')
+    Amazing Book Store
+@endsection
+
+@section('content')
+    <div class="row" >
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+    <div class="row" >
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+    <div class="row" >
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="img-thumbnail">
+                <img src="https://www.mobileread.com/forums/attachment.php?attachmentid=111264&d=1378642555" alt="..." class="img-fluid">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">12€</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+@endsection
+
